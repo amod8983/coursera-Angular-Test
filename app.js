439c45dba53ff25c8880174ffe58474b194114d8
Amod kumar <amod@coursera.com>
2018-03-05 20:19:08 +0530

    Added Function

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 (function(){
+  'use strict'
   var app=angular.module('myApp',[]);
   app.controller('myCtrl',function($scope){
-        $scope.name="Amod kumar";
+        $scope.name="";
+        $scope.charValue=0;
+        $scope.CalculateValue=function(){
+          var totalValue=charValueCalculator($scope.name);
+          $scope.charValue=totalValue;
+        
+        }
+
+        function charValueCalculator(string){
+          var num=0;
+          for(var i=0;i<string.length;i++){
+            num += string.charCodeAt(i);
+          }
+          return num;
+        }
   });
 })()
